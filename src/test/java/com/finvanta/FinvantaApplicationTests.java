baef6c5cb812c9da6c9e91a3dbdc7a995130ba32
Dnyaneshwar Ghodake <dnyaneshghodake@gmail.com>
2026-04-05 15:48:07 +0530

    Finvanta

---
+++ src/test/java/com/finvanta/FinvantaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finvanta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinvantaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
